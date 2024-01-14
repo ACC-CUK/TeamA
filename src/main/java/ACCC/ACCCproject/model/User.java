20db2a76517d7f8819af8082ec549bbe7cc68c5f
Joowon Lim <m4rinesnow34@gmail.com>
2024-01-14 21:58:18 +0900

    Feat: Mail 전송 구현

--- src/main/java/ACCC/ACCCproject/model/User.java
+++ src/main/java/ACCC/ACCCproject/model/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
+@Getter
 @Table(name = "users")
 public class User{
     @Id
--- src/main/java/ACCC/ACCCproject/service/UserService.java
+++ src/main/java/ACCC/ACCCproject/service/UserService.java
@@ -1,24 +1,31 @@
 package ACCC.ACCCproject.service;
 
+import static org.springframework.security.core.context.SecurityContextHolder.*;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 import java.util.Random;
 
 import ACCC.ACCCproject.controller.dto.GetEmailRes;
 import ACCC.ACCCproject.model.User;
 import ACCC.ACCCproject.repository.UserRepository;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-
+    private final JavaMailSender emailSender;
     private final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private final Random random = new Random();
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, JavaMailSender emailSender){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.emailSender = emailSender;
     }
 
     public boolean authenticateUser(String userEmail, String password){
@@ -41,9 +48,20 @@ public class UserService {
         user.setUserEmail(email);
         validEmail(user.getUserEmail());
         user.setRegistCode(generateRandomString(6));
+        sendEmail(user);
         userRepository.save(user);
         return new GetEmailRes(user.getUserEmail());
     }
+
+    private void sendEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(user.getUserEmail());
+        message.setSubject("ACCC 회원가입 인증 메일입니다.");
+        message.setText("인증번호는 " + user.getRegistCode() + "입니다.");
+        emailSender.send(message);
+    }
+
     private User getEmail(String email) {
         Optional<User> test = userRepository.findUserByuserEmail(email);
         return test.orElse(new User());
